68a02842fa78464e01e9c12ee4ebd0315f2e2a9b
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-24 03:18:24 +0300

    Finished project. Add cart 2.

--- lib/homescreentask.dart
+++ lib/homescreentask.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:work/utils/app_assets.dart';
 import 'package:work/utils/app_colors.dart';
-import 'package:work/widget/item_cart.dart';
+import 'package:work/widget/cart1/item_cart.dart';
+import 'package:work/widget/cart2/item_cart2.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = "homeScreen";
@@ -12,18 +14,36 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.64,
-          width: MediaQuery.of(context).size.width * 0.8,
-          decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: AssetImage(AppAssets.background),
-                fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height:0.64.sh,
+                width: 0.9.sw,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(AppAssets.background),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(30.r))),
+                child: const ItemCart(),
               ),
-              border: Border.all(color: Colors.white),
-              borderRadius: const BorderRadius.all(Radius.circular(30))),
-          child: const ItemCart(),
+              SizedBox(height: 0.25.sh,),
+              Container(
+                height: 0.64.sh,
+                width: 0.9.sw,
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage(AppAssets.backgroundCart),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30.r))),
+                child: const ItemCart2(),
+              ),
+            ],
+          ),
         ),
       ),
     );
