84a13bf7d023af86b212e1fae38b90d2a74cf60e
Mariam Shahir <mariamshahir2000@gmail.com>
2024-07-26 00:40:29 +0300

    Finished cart 5 screen.

---
+++ lib/widget/cart5/left_side_cart5.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:work/utils/aap_theme.dart';
+import 'package:work/utils/app_colors.dart';
+
+class LeftSideCart5 extends StatelessWidget {
+  const LeftSideCart5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 19.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Way of \nthe Cross",
+            style: AppTheme.title.copyWith(fontSize: 12.sp),
+          ),
+          const Spacer(),
+          Text(
+            "Natus dolores nobis ipsum et mollitia expedita eveniet velit."
+            "\nConsequatur facere est mollitia. Ea dolore tempore at id"
+            "\ndoloremque sapiente assumenda sed.",
+            style: AppTheme.textCart2.copyWith(fontSize: 4.sp),
+          ),
+          const Spacer(
+            flex: 5,
+          ),
+          Container(
+            width: 50.w,
+            height: 45.h,
+            decoration: BoxDecoration(
+              color: AppColors.buttonBackground,
+              borderRadius: BorderRadius.all(Radius.circular(20.r)),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.buttonBackground.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 5,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                "View More",
+                textAlign: TextAlign.center,
+                style: AppTheme.text.copyWith(fontSize: 5.sp),
+              ),
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
